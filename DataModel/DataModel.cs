0b7eed4d9bb9642ca1daf2ea1c261b954e5b1f38
Ahnessa Pleshchytser <89419679+AhnessP@users.noreply.github.com>
2022-09-07 14:31:31 +0300

    Update DataModel.cs

--- DataModel/DataModel.cs
+++ DataModel/DataModel.cs
@@ -5,7 +5,6 @@ public class DataModel
     public int TimeToWait { get; set; }
     public string Destination { get; set; }
     public string SiteUrl { get; set; }
-    
     public string Email { get; set; }
     public string InvalidEmail { get; set; }
     public string CheckInMonth { get; set; }
